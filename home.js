2c8301c7b7705d74153282f1daa1227507973ffd
Mozammel Hossain <shahrianapurbo@gmail.com>
2023-04-02 23:21:03 +0300

    v1

---
+++ home.js
@@ -0,0 +1,24 @@
+// selecting appBar
+var appBarTabLists = document.querySelectorAll(".appBarItem");
+appBarTabLists.forEach((tab) => {
+    tab.addEventListener("click", () => {
+        appBarTabLists.forEach((tab) => {
+            tab.classList.remove("selectedTab");
+        });
+        tab.classList.add("selectedTab");
+    });
+});
+
+// making app Bar invisible
+var appBar = document.querySelector(".appBar");
+var lastScrollY = window.scrollY;
+console.log(lastScrollY);
+
+window.addEventListener("scroll",()=>{
+    if (lastScrollY<window.scrollY) {
+        appBar.classList.add("hidden");
+    } else if (lastScrollY>window.screenY) {
+        appBar.classList.remove("hidden");
+    }
+    lastScrollY = window.scrollY;
+});
